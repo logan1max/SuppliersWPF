cc0a1f17a1acce28d002b7dee250f6582d140139
Максим Колосов <logan1.max@gmail.com>
2022-09-13 19:02:14 +0300

    Добавьте файлы проекта.

---
+++ SuppliersWPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+
+namespace SuppliersWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool Authourization(string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+            else
+            {
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
